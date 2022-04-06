16d4f777ff34a2322a784e3ee0d6855c18341271
Sunita Jennings <sunita.jennings@mx.com>
2022-04-06 02:03:09 -0600

    remaining api endpoints

--- node/controller.js
+++ node/controller.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-var user_guid = null
+var userGuid = null
 
 var listener = app.listen(port, function () {
     console.log('mx-quickstart node backend is listening on port ' + listener.address().port)
@@ -33,19 +33,31 @@ const configuration = new Configuration({
 })
 const client = new MxPlatformApi(configuration)
 
-app.get('/api/auth/:memberGuid', async function(request, response) {
-    const accountNumbersResponse = await client.listAccountNumbersByMember(request.params.memberGuid, user_guid)
-    response.json(accountNumbersResponse.data)
+app.get('/api/:memberGuid/status', async function(request, response) {
+    const statusResponse = await client.readMemberStatus(request.params.memberGuid, userGuid)
+    response.json (statusResponse.data)
 })
 
+app.get('/api/auth/:memberGuid', async function(request, response) {
+    const accountNumbersResponse = await client.listAccountNumbersByMember(request.params.memberGuid, userGuid)
+    response.json(accountNumbersResponse.data)
+})
+app.get('/api/balances', async function(request, response) {
+    const listUserAccountsResponse = await client.listUserAccounts(userGuid)
+    response.json(listUserAccountsResponse.data)
+})
+app.post('/api/balances', async function(request, response) {
+    const balancesResponse = await client.checkBalances(request.body.member_guid, request.body.user_guid)
+    response.json (balancesResponse.data)
+})
 app.post('/api/get_mxconnect_widget_url', async function(request, response) {
     const createUserRequestBody = {
         user: {
-            id: request.body.user_id
+            id: request.body.user_id ? request.body.user_id : null
         }
     }
     const createUserResponse = await client.createUser(createUserRequestBody)
-    user_guid = createUserResponse.data.user.guid
+    userGuid = createUserResponse.data.user.guid
 
     const widgetRequestBody = {
         widget_url : { 
@@ -58,6 +70,26 @@ app.post('/api/get_mxconnect_widget_url', async function(request, response) {
         }
     }
 
-    const widgetResponse = await client.requestWidgetURL(user_guid, widgetRequestBody)
+    const widgetResponse = await client.requestWidgetURL(userGuid, widgetRequestBody)
     response.json(widgetResponse.data)
+})
+app.get('/api/holdings', async function(request, response) {
+    const listHoldingsResponse = await client.listHoldings(userGuid)
+    response.json(listHoldingsResponse.data)
+})
+app.post('/api/holdings', async function(request, response) {
+    const aggregateMemberResponse = await client.aggregateMember(request.body.member_guid, request.body.user_guid)
+    response.json(aggregateMemberResponse.data)
+})
+app.get('/api/identity/:memberGuid', async function(request, response) {
+    const listAccountOwnersResponse = await client.listAccountOwnersByMember(request.params.memberGuid, userGuid)
+    response.json(listAccountOwnersResponse.data)
+})
+app.post('/api/identity/:memberGuid', async function(request, response) {
+    const identifyMemberResponse = await client.identifyMember(request.params.memberGuid, userGuid)
+    response.json(identifyMemberResponse.data)
+})
+app.get('/api/transactions/:memberGuid', async function (request, response) {
+    const listTransactionsResponse = await client.listTransactionsByMember(request.params.memberGuid, userGuid)
+    response.json (listTransactionsResponse.data)
 })
