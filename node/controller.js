8448e21ae43389a4063ef1b7e398c287f1582a96
Sunita Jennings <sunita.jennings@mx.com>
2022-04-06 01:16:12 -0600

    widget load works - still need to add remaining endpoints

---
+++ node/controller.js
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+import { Configuration, MxPlatformApi } from 'mx-platform-node';
+
+dotenv.config({ path: '../.env' })
+var port = process.env.PORT || 8000
+
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+var listener = app.listen(port, function () {
+    console.log('mx-quickstart node backend is listening on port ' + listener.address().port)
+})
+
+const configuration = new Configuration({
+    // Configure with your Client ID/API Key from https://dashboard.mx.com
+    username: process.env.CLIENT_ID,
+    password: process.env.API_KEY,
+
+    // Configure environment. https://int-api.mx.com for development, https://api.mx.com for production
+    basePath: 'https://int-api.mx.com',
+
+    baseOptions: {
+        headers: {
+            Accept: 'application/vnd.mx.api.v1+json'
+        }
+    }
+})
+const client = new MxPlatformApi(configuration)
+
+app.post('/api/get_mxconnect_widget_url', async function(request, response) {
+    const createUserRequestBody = {
+        user: {
+            id: request.body.user_id
+        }
+    }
+    const createUserResponse = await client.createUser(createUserRequestBody)
+
+    const widgetRequestBody = {
+        widget_url : { 
+            include_transactions: true,
+            is_mobile_webview: false,
+            mode: 'verification',
+            ui_message_version: 4,
+            wait_for_full_aggregation: true,
+            widget_type: 'connect_widget'
+        }
+    }
+
+    const widgetResponse = await client.requestWidgetURL(createUserResponse.data.user.guid, widgetRequestBody)
+    response.json(widgetResponse.data)
+})
