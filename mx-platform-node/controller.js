06198903daafa33fb082dc1541a95f7d3380c204
Sunita Jennings <sunita.jennings@gmail.com>
2022-04-13 21:45:11 -0600

    Add node backend (#8)
    * if ContentType is not set, node won't parse body
    
    * widget load works - still need to add remaining endpoints
    
    * account numbers/auth endpoint
    
    * content-type header
    
    * remaining api endpoints
    
    * lock files
    
    * renamed node backend package - in case we want a non-client lib route
    
    * try/catch on all client lib/api calls
    
    * how to run the node backend
    
    * Add error UI on endpoints and ruby config help
    
    * Make endpoints match PAPI
    
    * Use spinner on button load
    
    * Merged incoming userId change  with: if ContentType is not set, node won't parse body
    
    * widget load works - still need to add remaining endpoints
    
    * account numbers/auth endpoint
    
    * remaining api endpoints
    
    * lock files
    
    * renamed node backend package - in case we want a non-client lib route
    
    * try/catch on all client lib/api calls
    
    * how to run the node backend
    
    * updated api layer to match frontend+ruby changes
    
    * fixed bad userId reference
    
    * updated api layer, working
    
    * Update mx-platform-node/controller.js
    
    Co-authored-by: Logan Rasmussen <logan.rasmussen@mx.com>
    
    * 2 space indentation, newlines between app.get() calls
    
    * cleaned up error logging
    
    Co-authored-by: Sunita Jennings <sunita.jennings@mx.com>
    Co-authored-by: Craig Tingey <craig.tingey@mx.com>
    Co-authored-by: Logan Rasmussen <logan.rasmussen@mx.com>

---
+++ mx-platform-node/controller.js
@@ -0,0 +1,137 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+import { Configuration, MxPlatformApi } from 'mx-platform-node';
+
+dotenv.config({ path: '../.env' })
+var port = process.env.PORT || 8000
+
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.listen(port, function () {
+  console.log(`mx-quickconnect node backend is listening on port ${port}`)
+})
+
+const configuration = new Configuration({
+  // Configure with your Client ID/API Key from https://dashboard.mx.com
+  username: process.env.CLIENT_ID,
+  password: process.env.API_KEY,
+
+  // Configure environment. https://int-api.mx.com for development, https://api.mx.com for production
+  basePath: 'https://int-api.mx.com',
+
+  baseOptions: {
+    headers: {
+      Accept: 'application/vnd.mx.api.v1+json'
+    }
+  }
+})
+
+const client = new MxPlatformApi(configuration)
+
+app.post('/api/get_mxconnect_widget_url', async function (request, response) {
+  const createUserRequestBody = {
+    user: {
+      id: request.body.user_id ? request.body.user_id : null
+    }
+  }
+
+  try {
+    const createUserResponse = await client.createUser(createUserRequestBody)
+
+    let userGuid = createUserResponse.data.user.guid
+
+    const widgetRequestBody = {
+      widget_url: {
+        include_transactions: true,
+        is_mobile_webview: false,
+        mode: 'verification',
+        ui_message_version: 4,
+        wait_for_full_aggregation: true,
+        widget_type: 'connect_widget'
+      }
+    }
+
+    const widgetResponse = await client.requestWidgetURL(userGuid, widgetRequestBody)
+    response.json(widgetResponse.data)
+  } catch (e) {
+    logAndReturnApiError("requestWidgetURL", e, response)
+  }
+})
+
+app.get('/users/:userGuid/members/:memberGuid/check_balance', async function (request, response) {
+  try {
+    const listUserAccountsResponse = await client.listUserAccounts(request.params.userGuid)
+    response.json(listUserAccountsResponse.data)
+  } catch (e) {
+    logAndReturnApiError("listUserAccounts", e, response)
+  }
+})
+
+app.post('/users/:userGuid/members/:memberGuid/check_balance', async function (request, response) {
+  try {
+    const balancesResponse = await client.checkBalances(request.params.memberGuid, request.params.userGuid)
+    response.json(balancesResponse.data)
+  } catch (e) {
+    logAndReturnApiError("checkBalances", e, response)
+  }
+})
+
+app.get('/users/:userGuid/members/:memberGuid/status', async function (request, response) {
+  try {
+    const statusResponse = await client.readMemberStatus(request.params.memberGuid, request.params.userGuid)
+    response.json(statusResponse.data)
+  } catch (e) {
+    logAndReturnApiError("readMemberStatus", e, response)
+  }
+})
+
+app.get('/users/:userGuid/members/:memberGuid/verify', async function (request, response) {
+  try {
+    const listAccountNumbersResponse = await client.listAccountNumbersByMember(request.params.memberGuid, request.params.userGuid)
+    response.json(listAccountNumbersResponse.data)
+  } catch (e) {
+    logAndReturnApiError("listAccountNumbersByMember", e, response)
+  }
+})
+
+app.get('/users/:userGuid/members/:memberGuid/identify', async function (request, response) {
+  try {
+    const listAccountOwnersResponse = await client.listAccountOwnersByMember(request.params.memberGuid, request.params.userGuid)
+    response.json(listAccountOwnersResponse.data)
+  } catch (e) {
+    logAndReturnApiError("listAccountOwnersResponse", e, response)
+  }
+})
+
+app.post('/users/:userGuid/members/:memberGuid/identify', async function (request, response) {
+  try {
+    const identifyMemberResponse = await client.identifyMember(request.params.memberGuid, request.params.userGuid)
+    response.json(identifyMemberResponse.data)
+  } catch (e) {
+    logAndReturnApiError("identifyMember", e, response)
+  }
+})
+
+app.get('/users/:userGuid/members/:memberGuid/transactions', async function (request, response) {
+  try {
+    const listTransactionsResponse = await client.listTransactionsByMember(request.params.memberGuid, request.params.userGuid)
+    response.json(listTransactionsResponse.data)
+  } catch (e) {
+    logAndReturnApiError("listTransactionsByMember", e, response)
+  }
+})
+
+function logAndReturnApiError(method, e, response) {
+  if (!e.response) {
+    console.log("Error when calling MxPlatformApi->" + method + ": " + e)
+    response.status("500").send({ 'errorMessage': e })
+    return
+  }
+  console.log("Error when calling MxPlatformApi->" + method + ": HTTP " + e.response.status + " " + e.response.statusText)
+  console.log(e.response.data)
+  response.status(e.response.status).send({ 'errorMessage': e.response.data })
+}
