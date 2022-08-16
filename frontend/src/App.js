3a3ce80fa39566df339de2ab5ed1f698f9a4c0c1
Sunita Jennings <sunita.jennings@gmail.com>
2022-08-16 14:38:10 -0600

    sunita/resolve eslint warnings (#31)
    * isLoading and setIsLoading don't need to be props, can use local state
    
    * strict (in)equality operator
    
    Co-authored-by: Sunita Jennings <sunita.jennings@mx.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,18 @@ import UserEndpoints from "./components/UserEndpoints";
 function App() {
   const [userGuid, setUserGuid] = useState(null);
   const [memberGuid, setMemberGuid] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
 
 
   return (
     <div className="App">
       <div className="body">
         {userGuid === null && memberGuid === null ? (
-          <LaunchButton isLoading={isLoading} setIsLoading={setIsLoading} setUserGuid={setUserGuid} setMemberGuid={setMemberGuid} />
+          <LaunchButton setUserGuid={setUserGuid} setMemberGuid={setMemberGuid} />
         ) :
-        (
-          <UserEndpoints userGuid={userGuid} memberGuid={memberGuid} />
-        )
-      }
+          (
+            <UserEndpoints userGuid={userGuid} memberGuid={memberGuid} />
+          )
+        }
       </div>
     </div>
   );
