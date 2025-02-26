22645874c4dc52ab1acbc4f7802932569a1dd58f
Craig Tingey <craig.tingey@mx.com>
2025-02-26 13:59:58 -0700

    Update LaunchButton.js
    Updates link to mx docs

--- frontend/src/components/LaunchButton.js
+++ frontend/src/components/LaunchButton.js
@@ -209,7 +209,7 @@ function LaunchButton({ setUserGuid, setMemberGuid }) {
                   password
                 </td>
                 <td>
-                  correct, challenge, options, image, <a href="https://docs.mx.com/api/guides/testing#test_credentials" target="_blank" rel="noreferrer">see docs for more scenarios</a>
+                  correct, challenge, options, image, <a href="https://docs.mx.com/resources/test-platform/mxbank/#mx-bank-testing-scenarios" target="_blank" rel="noreferrer">see docs for more scenarios</a>
                 </td>
               </tr>
             </tbody>
